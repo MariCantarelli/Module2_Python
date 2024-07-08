4929f78d7d960921d347bc4f9a64ee8b04ec37ad
Marina Cantarelli <macbarroca@gmail.com>
2024-07-08 14:17:48 -0300

    Math operations

---
+++ Class1.py
@@ -0,0 +1,9 @@
+#Math operations
+
+Calculation = 2 ** 3 + 4 
+#(2 + 2) * 3 / 2 
+print(Calculation)
+
+#Exponenciais 
+#Parenteses!
+#Multiplication and divison cames first than addition and substraction.
