4648ef7cf529b1f78ed1c5fdb944e9bb3a019d9b
Marina Cantarelli <macbarroca@gmail.com>
2024-07-08 14:30:45 -0300

    Comparison Operators

---
+++ Class2.py
@@ -0,0 +1,27 @@
+#Comparison Operators
+
+#Operators = 10 == 10
+#Operators = 10 == 11
+#Operators = 'banana' == 'banana'
+#Operators = 'banana' == 'Banana'
+#Operators = 'banana' != 'Banana'
+#Operators = 'banana' != 'banana'
+#Operators = 10 > 13
+#Operators = 10 > 9
+#Operators = 10 < 9
+#Operators = 10 < 93
+#Operators = 10 <= 93
+#Operators = 10 <= 9
+Operators = 10 <= 10
+print(Operators)
+
+
+
+
+
+# == Equal
+# != Not equal
+# > Greater than 
+# < Less than
+# >= Greater than or equal to
+# <= Less than or equal to 
